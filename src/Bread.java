dbac0573295a3c2fac55ae5625cc7f90ec8e6300
Leclie <07140714no@gmail.com>
2023-03-28 19:49:29 +0300

    1 hw

---
+++ src/Bread.java
@@ -0,0 +1,13 @@
+public class Bread extends Product{
+
+    private double calories;
+    public Bread(String name, double price, int value, double calories) {
+        super(name, price, value);
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Хлеб : %s, калорийность : %f" ,super.toString(), calories);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        VendingMachine mart = new VendingMachine();
+        mart.addProduct(new Milk("moloko", 50, 2))
+                .addProduct(new Product("cheese", 100.2, 5))
+                .addProduct(new ConcentrateMilk("Советская", 150, 1))
+                .addProduct(new Chocolate("Аленка", 35.20,3, "Ореховый"))
+                .addProduct(new Bread("хлеб", 50, 5, 265));
+
+        System.out.println("До продажи");
+        System.out.println(mart);
+        PrintSell(mart,"moloko");
+        PrintSell(mart,"moloko");
+        PrintSell(mart,"moloko");
+        PrintSell(mart,"хлеб");
+        System.out.println();
+        System.out.println("После продажи");
+        System.out.println(mart);
+        System.out.println();
+        PrintSell(mart,"Фиговина");
+        System.out.println(mart);
+    }
+
+    public static void PrintSell (VendingMachine machine, String nameProd){
+        try{
+            System.out.println(machine.sell(nameProd));
+        } catch (Exception e) {
+            System.out.println("Товар не найден");
+        }
+    }
+}
